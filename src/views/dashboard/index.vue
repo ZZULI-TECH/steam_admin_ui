6e52a8f4288b62cd55883dcd113119825ebca244
lyfZhixing <2214974097@qq.com>
2019-05-02 12:18:47 +0800

    init admin-ui

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="dashboard-container">
+    <h1>模版首页</h1>
+    <div class="dashboard-text">登陆用户名:{{ name }}</div>
+    <div class="dashboard-text">登陆用户角色:<span v-for="role in roles" :key="role">{{ role }}</span></div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name',
+      'roles'
+    ])
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
